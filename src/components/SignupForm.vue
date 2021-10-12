88e7d63b50276bf312366a10c59c892545a0e8e3
Gideon-Daniels <gideondaniels.dragoonix@gmail.com>
2021-10-12 10:15:44 +0200

    update-Keyboard events modifiers

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -12,10 +12,17 @@
             <option value="designer">Web Designer</option>
         </select>
 
+        <label>Skills:</label>
+        <input type="text" v-model="tempSkill" @keyup.alt="addSkill">
+        <div v-for="skill in skills" :key="skill" class="pill">
+            {{ skill }}
+        </div>
+
         <div class="terms">
             <input type="checkbox" required v-model="terms">
             <label>Accept terms and conditions</label>
         </div>
+
 <!-- multiple check boxes -->
         <!-- <div>
             <input type="checkbox" value="shaun" v-model="names">
@@ -38,7 +45,7 @@
     <p> Password : {{ password }}</p>
     <p> Role : {{ role }}</p>
     <p> Terms accepted : {{ terms }}</p>
-    <p> Names : {{ names }}</p>
+    <!-- <p> Names : {{ names }}</p> -->
 </template>
 
 <script>
@@ -50,6 +57,18 @@ export default {
          role: 'developer',
          terms: false,
         //  names: []
+        tempSkill: '',
+        skills: []
+     }
+ },
+ methods: {
+    addSkill(e){
+        if(e.key === ',' && this.tempSkill){
+            if(!this.skills.includes(this.tempSkill)){
+                this.skills.push(this.tempSkill)
+            }
+            this.tempSkill = ''
+        }
      }
  }
 }
